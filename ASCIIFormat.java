c72559452ef81d2b259aec48c3e372a0e0475184
Mayank4545 <keshri.mayank@gmail.com>
2016-11-20 16:35:02 +0530

    Assignment 1.4
     Program to print the characters corresponding to ASCII codes from 65 to 90

---
+++ ASCIIFormat.java
@@ -0,0 +1,12 @@
+public class ASCIIFormat {
+public static void main(String[] args) {
+	
+	int num = 65;
+	for(num = 65; num <= 90; num++){   /* 65 = A in ASCII */
+		char ascii = (char) num;  
+		System.out.println(num + " - " + ascii);
+	}	
+}
+}
+
+
